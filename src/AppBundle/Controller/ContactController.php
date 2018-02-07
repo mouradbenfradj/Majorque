dc54fef021909c4a56d3eb055dee27063fbb10ae
Baobab <webdev.boabebmediagroup@gmail.com>
2018-02-07 14:35:28 +0100

    mail

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -16,12 +16,34 @@ class ContactController extends Controller
         // replace this example code with whatever you need
         return $this->render('contact/contact.html.twig');
     }
+
     /**
      * @Route("/sendmail", name="sendmail")
      */
-    public function sendEmailAction(Request $request)
+    public function sendEmailAction(Request $request)//($name, $email, $phone, $subject, $message)
     {
-        // replace this example code with whatever you need
+        $message = (new \Swift_Message($request->get('subject')))
+            ->setFrom($request->get('email'))
+            ->setTo('mourad.benfradj.sidratsoft@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    'contact/message.html.twig',
+                    array('name'=>$request->get('name'),'phone'=>$request->get('phone'),'message' => $request->get('message')))
+                ,
+                'text/html'
+            )/*
+             * If you also want to include a plaintext version of the message
+            ->addPart(
+                $this->renderView(
+                    'Emails/registration.txt.twig',
+                    array('name' => $name)
+                ),
+                'text/plain'
+            )
+            */
+        ;
+
+        $this->get('mailer')->send($message);
         return $this->render('contact/contact.html.twig');
     }
 }
